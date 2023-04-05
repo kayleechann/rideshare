8709bbcffd945b25479ed13e24ed6b5abfb4a1e4
enguyen20 <eminguyen@chapman.edu>
2023-04-05 00:52:36 -0700

    Functionality to connect a rider with driver and create a new ride

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from helper import helper
-from db_operations import db_operations
 from datetime import date
+from datetime import datetime
 
 # import MySQL
 import mysql.connector
@@ -13,47 +13,49 @@ conn = mysql.connector.connect(host="localhost",
     database="RideShare")
 
 # create cursor object
-cur_obj = conn.cursor()
+cur_obj = conn.cursor(buffered=True)
 
-"""  
+"""
 # create rider table
 query = '''
     CREATE TABLE rider(
         riderID INT NOT NULL PRIMARY KEY,
-        fullName VARCHAR(40)
-        creationDate DATETIME
-    );
+        fullName VARCHAR(40),
+        creationDate DATE
+    )
 '''
 cur_obj.execute(query)
 conn.commit()
 print('Rider Table Created')
 
 # create driver table
-query = '''
+query2 = '''
     CREATE TABLE driver(
         driverID INT NOT NULL PRIMARY KEY,
         fullName VARCHAR(40),
         rating DECIMAL,
         licensePlate VARCHAR(7),
         driverMode BOOLEAN
-    );
+    )
 '''
-cur_obj.execute(query)
+cur_obj.execute(query2)
 conn.commit()
 print('Driver Table Created')
 
 # create rides table
-query = '''
+query3 = '''
     CREATE TABLE rides(
         rideID INT NOT NULL PRIMARY KEY,
         pickupLocation VARCHAR(40),
         dropoffLocation VARCHAR(40),
         dateAndTime DATETIME,
+        riderID INT,
+        driverID INT,
         FOREIGN KEY (riderID) REFERENCES rider(riderID),
         FOREIGN KEY (driverID) REFERENCES driver(driverID)
-    );
+    )
 '''
-cur_obj.execute(query)
+cur_obj.execute(query3)
 conn.commit()
 print('Rides Table Created')
 """
@@ -118,6 +120,7 @@ def highest_id():
 # check if the user is a rider or driver
 def check_user():
     while True:
+        global userID
         userID = input("What is your riderID or driverID?\n")
         
         # check if userID is a rider
@@ -145,16 +148,94 @@ def check_user():
         else:
             print("Incorrect userID. Try Again.")
 
+# shows the driver their options
+def driver_menu():
+    print('''Select from the following menu options:
+    1. View current rating
+    2. View all rides you have driven for
+    3. Change driver mode
+        ''')
+    return helper.get_choice([1,2,3])
+
+def rider_menu():
+    print('''Select from the following menu options:
+    1. View all past rides
+    2. Find a driver
+    3. Rate a driver
+          ''')
+    return helper.get_choice([1,2,3])
+
+def current_rating():
+    query = '''
+    SELECT rating
+    FROM driver
+    '''
+    cur_obj.execute(query)
+    return cur_obj.fetchone()[0]
+
+def change_driver_mode():
+    print("Would you like to activate (1) or deactivate (0) driver mode?")
+    choice = helper.get_choice([0,1])
+    
+    query = '''
+    UPDATE driver
+    SET driverMode = %s
+    WHERE driverID = %s
+    '''
+    cur_obj.execute(query, (choice, userID))
+    conn.commit()
+
+# find rider a driver and create a ride
+def find_driver():
+    # find a driver that has driver mode activated
+    query = '''
+    SELECT driverID
+    FROM driver
+    WHERE driverMode = 1
+    '''
+    cur_obj.execute(query)
+    driverID = cur_obj.fetchone()[0]
+    print("Driver connected!")
+    
+    pickup = input("Enter pickup location: ")
+    dropoff = input("Enter dropoff location: ")
+    
+    rideID = new_rideID()
+    now = datetime.now()
+    
+    # create a ride
+    query2 = '''
+    INSERT INTO rides
+    VALUES(%s,%s,%s,%s,%s,%s)
+    '''
+    cur_obj.execute(query2, (rideID, pickup, dropoff, now, userID, driverID))
+    conn.commit()
+    
+# find an unused rideID
+def new_rideID():
+    # find the highest id number in the rides table
+    query = '''
+    SELECT MAX(rideID)
+    FROM rides
+    '''
+    cur_obj.execute(query)
+    max_riderID = cur_obj.fetchone()[0]
+    
+    new_id = max_riderID + 1
+    return new_id
+    
+
 #main program
 startScreen()
 
 # ask user if they are a new or returning user
 user_type = print("Are you a new (1) or returning user (2)?")
 num = helper.get_choice([1,2])
+
 # if new, ask if rider or driver
 if num == 1:
-    add_user()
     # make an account for respective user
+    add_user()
 # else, ask the user for their userID
 else:
     # determine if they are a rider or driver
@@ -162,14 +243,30 @@ else:
     # if user_type = 0, user is a driver
     user_type = check_user()
     if user_type == 1:
-        print("rider")
+        user_choice = rider_menu()
+        if user_choice == 1:
+            # view all rides
+            print("rides: ")
+        if user_choice == 2:
+            # find a driver
+            find_driver()
     else:
-        print("driver")
+        user_choice = driver_menu()
+        if user_choice == 1:
+            # view current rating
+            rating = current_rating()
+            print("Your current rating: " + str(rating))
+        if user_choice == 2:
+            # view all rides
+            print("rides: ")
+        if user_choice == 3:
+            # change driver mode
+            change_driver_mode()
 
 # if user is a driver, show the following options:
-    # 1. view their current rating
+    # 1. view their current rating DONE
     # 2. view all rides they have driven for
-    # 3. driver mode (activate/deactivate)
+    # 3. driver mode (activate/deactivate) DONE
     
 # if user is a rider, show the following options:
     # 1. view all rides they have taken
